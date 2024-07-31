6e8af5f5db89d7e37ec7c71196d24715727fdadb
joreynajr <joreyna@live.com>
2024-07-31 11:01:55 -0700

    Track the fast motif analysis analysis

---
+++ workflow/scripts/pipeline/aggregate_simulations_pvalues.fast.commander.sh
@@ -0,0 +1,24 @@
+
+###############################################################################
+# run with bash
+###############################################################################
+samplesheet="results/samplesheet/motif_pairs/samplesheet.txt"
+#bash workflow/scripts/pipeline/aggregate_simulations_pvalues.qarray.qsh $samplesheet 1
+
+###############################################################################
+# run with SLURM
+###############################################################################
+samplesheet="results/samplesheet/motif_pairs/samplesheet.txt"
+export_vars="samplesheet=$samplesheet"
+
+# set output directoreis 
+output_log="results/motif_pairs/chrom_correction_only.fast/logs/agg_simulations/agg_simulations.%A.%a.out"
+error_log="results/motif_pairs/chrom_correction_only.fast/logs/agg_simulations/agg_simulations.%A.%a.out"
+mkdir -p $(dirname $output_log)
+
+# submit jobs 
+sbatch --array=2-53 \
+        --export="$export_vars" \
+        --output "$output_log" \
+        --error "$error_log" \
+        workflow/scripts/pipeline/aggregate_simulations_pvalues.fast.qarray.qsh
