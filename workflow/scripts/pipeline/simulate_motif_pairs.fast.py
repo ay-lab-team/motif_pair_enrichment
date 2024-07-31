f2a06c00ceff8137638513d51a65ced678242f81
joreynajr <joreyna@live.com>
2024-07-31 12:30:58 -0700

    Updating code to remove blacklisted regions

---
+++ workflow/scripts/pipeline/simulate_motif_pairs.fast.py
@@ -0,0 +1,252 @@
+import itertools as it
+import pandas as pd
+from collections import defaultdict, Counter
+import json
+import numpy as np
+import time
+import argparse
+import pybedtools as pbt
+pbt.set_bedtools_path('/mnt/BioApps/bedtools/bin')
+
+###############################################################################
+# Setup the commandline interface
+###############################################################################
+parser = argparse.ArgumentParser()
+parser.add_argument('--inputpath', type=str, required=True)
+parser.add_argument('--savepath', type=str, required=True)
+parser.add_argument('--sims', type=int, required=True)
+parser.add_argument('--account_dist', type=str, required=True, default="Yes")
+parser.add_argument('--distance', type=int, required=False, default=2000000)
+parser.add_argument('--blacklist', type=str, required=False, default=None)
+args = parser.parse_args()
+
+inputpath = args.inputpath
+savepath = args.savepath
+sims = args.sims
+account_dist = args.account_dist
+within_dist = args.distance
+blacklist_path = args.blacklist
+
+# process WITHOUT the blacklist regions
+if blacklist_path is None:
+    df = pd.read_csv(inputpath, sep = '\t', header=0)
+    filter_df =  df.loc[(~df['motif_name_1'].isna()) & (~df['motif_name_2'].isna())].reset_index(drop=True)
+
+# process WITH the blacklist regions
+else:
+    
+    # loading pbt's of loops and blacklisted regions
+    loops_pbt = pbt.BedTool(inputpath)
+    blacklist_pbt = pbt.BedTool(blacklist_path)
+
+    # extract loops with no blacklist regions
+    loops_without_blk = loops_pbt.pair_to_bed(blacklist_pbt, type='neither')
+    loops_without_blk = loops_without_blk.to_dataframe(header=0, disable_auto_names=True)
+    columns = ['chr1', 'start1', 'end1', 'chr2', 'start2', 'end2',
+            'motif_ID_1', 'motif_name_1', 'motif_ID_2', 'motif_name_2']
+    loops_without_blk.columns = columns
+
+    # drop any anchors with no motifs in either anchor (contains NaN)
+    filter_df =  loops_without_blk.loc[(~loops_without_blk['motif_name_1'].isna()) & (~loops_without_blk['motif_name_2'].isna())].reset_index(drop=True)
+
+# add the anchor names
+def create_anchor_name(sr, anchor_no):
+    anchor_name = '{}-{}-{}'.format(sr[f'chr{anchor_no}'], sr[f'start{anchor_no}'], sr[f'end{anchor_no}'])
+    return(anchor_name)
+filter_df.loc[:, 'anchor_name1'] = filter_df.apply(create_anchor_name, args=[1], axis=1)
+filter_df.loc[:, 'anchor_name2'] = filter_df.apply(create_anchor_name, args=[2], axis=1)
+
+###############################################################################
+# Counting Observed Motif Pairs
+###############################################################################
+print('# Counting Observed')
+
+def sort_motifs(motifs):
+    sorted_motifs = tuple(sorted(motifs))
+    return sorted_motifs
+
+# Set up counter
+motif_pair_counter = Counter()
+
+# Loop through all enteries in dataframe
+uniq_motif_pairs = []
+for num in range(len(filter_df)):
+
+    # Get motifs 1
+    motifs1 = filter_df.motif_name_1[num].split(',')
+    
+    # Get motifs 1
+    motifs2 = filter_df.motif_name_2[num].split(',')
+    
+    # Form the combination of all motif1 and motif2 pairs by
+    # using the cross product
+    combos = it.product(motifs1, motifs2)
+    
+    # Organize motifs into alphabetical order and drop duplicates
+    combos = [sort_motifs(tmotifs) for tmotifs in combos]
+    combos = sorted(set(combos))
+    
+    # Record them in counter
+    for p in combos:
+        motif_pair_counter[p] += 1
+        uniq_motif_pairs.append(p)
+        
+uniq_motif_pairs = list(set(uniq_motif_pairs))
+
+###############################################################################
+# Construct Anchor Slots and Dictionary
+###############################################################################
+# An anchor slot is composed of a genetic region corresponding to a loop anchor
+# and its constitutent motifs as a list. These are stored in dictionarys as:
+# {'chr1-10000-15000': ['ACT', 'GGG', 'CGT']}. These anchor slots are used for
+# simulation steps where the anchor slots are randomized.
+print('# Construct Anchor Slots')
+
+# Initialize the anchor slots data structure
+anchor_slots = {}
+anchor_shuffle_dict = {}
+anchor_ids = []
+motifs_count_sims = []
+
+# Loop through all entries in dataframe
+for i, sr in filter_df.iterrows():
+
+    # Create anchor ids
+    anchor1_id = sr['anchor_name1']
+    anchor2_id = sr['anchor_name2']
+
+    # Create blank list of anchors for shuffling based on distance
+    entry_shuffle = []
+    
+    # Get chromosome
+    chr1 = sr['chr1']
+    chr2 = sr['chr2']
+
+    # Put anchor ids and slots into list
+    if anchor1_id not in anchor_slots:
+        
+        # Create the current slot
+        motifs1 = list(sr.motif_name_1.split(','))
+        anchor_slots[anchor1_id] = list(set(motifs1))
+        anchor_ids.append(anchor1_id)
+
+        # For the given slot, determine what anchors can be shuffled with it. If distance has been
+        # provided then this will be applied.
+        # Create column in dataframe to get distance measure for anchor 1
+        # JR note: how is this calculating distance? Why are we using start1 of filter_df with start1 of sr?
+        # JR note: I think I see how the distance is being calculated but you would have to check the end as well!
+        filter_df['tmp_dist'] = abs(filter_df['start1'] - sr['start1'])
+        if account_dist == 'Yes':
+
+            # Only keep anchors within certain distance and within certain chromosome
+            tmp_dist_df = filter_df[(filter_df['tmp_dist'] <= within_dist) & \
+                                    (filter_df['chr1'] == chr1) & \
+                                    (filter_df['chr2'] == chr2)].copy()
+
+        else:
+            # Only keep motifs within specific chromosome
+            tmp_dist_df = filter_df[(filter_df['chr1'] == chr1) & \
+                                    (filter_df['chr2'] == chr2)].copy()
+        
+        # Create anchor id for anchors within distance of anchor 1
+        tmp_dist_df['anchor1_id_name'] = tmp_dist_df['chr1'] + '-' + \
+                                            tmp_dist_df['start1'].astype('str') + '-' + \
+                                            tmp_dist_df['end1'].astype('str')
+        entry_shuffle += list(tmp_dist_df['anchor1_id_name'])
+
+        # Create list of anchors wihin distance
+        anchor_shuffle_dict[anchor1_id] = entry_shuffle
+
+        # Get summary stats of amount of motifs in anchors
+        motifs_count_sims.append(len(entry_shuffle))
+    
+    if anchor2_id not in anchor_slots:
+        
+        motifs2 = list(sr.motif_name_2.split(','))
+        anchor_slots[anchor2_id] = list(set(motifs2))
+        anchor_ids.append(anchor2_id)
+        
+        # Create column in dataframe to get distance measure for anchor 1
+        filter_df['tmp_dist'] = abs(filter_df['start2'] - sr['start2'])
+        if account_dist == 'Yes':
+
+            # Only keep anchors within certain distance and within certain chromosome
+            tmp_dist_df = filter_df[(filter_df['tmp_dist'] <= within_dist) & \
+                                    (filter_df['chr1'] == chr1) & \
+                                    (filter_df['chr2'] == chr2)].copy()
+
+        else:
+            # Only keep motifs within specific chromosome
+            tmp_dist_df = filter_df[(filter_df['chr1'] == chr1) & \
+                                    (filter_df['chr2'] == chr2)].copy()
+
+        # Create anchor id for anchors within distance of anchor 2
+        tmp_dist_df['anchor2_id_name'] = tmp_dist_df['chr2'] + '-' + \
+                                            tmp_dist_df['start2'].astype('str') + '-' + \
+                                            tmp_dist_df['end2'].astype('str')
+        entry_shuffle += list(tmp_dist_df['anchor2_id_name'])
+
+        # Create list of anchors within distance
+        anchor_shuffle_dict[anchor2_id] = entry_shuffle
+
+        # Get summary stats of amount of motifs in anchors
+        motifs_count_sims.append(len(entry_shuffle))
+
+
+###############################################################################
+# Conduct Simulations
+###############################################################################
+print('# Conduct Simulations')
+
+sim_results = [Counter() for sim in range(sims)]
+sim_idx = 0
+num_loops = len(filter_df)
+
+sims_motif_pair_counter = Counter()
+for anchor_name1, anchor_name2 in filter_df[['anchor_name1', 'anchor_name2']].values:
+
+    # Simulate anchors based on chromsome and distance
+    sim_anchors1 = np.random.choice(anchor_shuffle_dict[anchor_name1], size=sims, replace=True) 
+    sim_anchors2 = np.random.choice(anchor_shuffle_dict[anchor_name2], size=sims, replace=True) 
+
+    sim_idx = 0
+    for (slot1, slot2) in zip(sim_anchors1, sim_anchors2):
+
+        # Get motifs from simulated anchors
+        # To remove duplicates motif pairs, motifs in anchor1 are removed from motifs in anchor 2
+        sim_motifs1 = anchor_slots[slot1]
+        sim_motifs2 = anchor_slots[slot2]
+
+        # Form the combination of all motif1 and motif2 pairs by
+        # using the cross product
+        shuffle_combos = it.product(sim_motifs1, sim_motifs2)
+
+        # Organize motifs into alphabetical order
+        shuffle_combos = [sort_motifs(tmotifs) for tmotifs in shuffle_combos]
+        shuffle_combos = sorted(set(shuffle_combos))
+
+        # Record them in counter
+        for p in shuffle_combos:
+            sim_results[sim_idx][p] += 1 
+                
+        # Update counter
+        sim_idx += 1
+                                                            
+
+###############################################################################
+# Save json file to output
+###############################################################################
+print('# Save json file to output')
+
+# Keys are from observed
+# Aggregate simulations into one list only for observed motif pairs   
+mp_sim_aggs = defaultdict(list)
+for sim_idx in range(sims):
+    for mp in uniq_motif_pairs:
+        mp_sim_aggs[str(mp)].append(sim_results[sim_idx][mp])
+
+# Save json
+json_d = json.dumps(mp_sim_aggs)
+with open(savepath, 'w') as fw:
+    fw.write(json_d)
+
---
+++ workflow/scripts/pipeline/simulate_mps_fast_with_blacklist/simulate_motif_pairs.commander.sh
@@ -0,0 +1,33 @@
+
+# SLURM_ARRAY_TASK_IDs are used for each batch rather than samples
+
+# run with bash
+#samplesheet="results/samplesheet/motif_pairs/samplesheet.txt"
+#sample_name=$(sed -n '1p' $samplesheet)
+#bash workflow/scripts/pipeline/simulate_motif_pairs.fast.within_chroms.qarray.qsh $samplesheet $sample_name 1 
+
+###############################################################################
+# run with qsub
+###############################################################################
+samplesheet="results/samplesheet/motif_pairs/samplesheet.txt"
+#for i in $(seq 1 53);
+for i in $(seq 1 53);
+do
+    sample_name=$(sed -n "${i}p" $samplesheet)
+    echo $sample_name
+
+    # setting the variables for exporting
+    export_vars="samplesheet=$samplesheet,sample_name=$sample_name"
+
+    # setting the logs
+    output_log="results/motif_pairs/chrom_correction_only.fast.with_blacklist/logs/${sample_name}/run_simulations.%A.%a.out"
+    error_log="results/motif_pairs/chrom_correction_only.fast.with_blacklist/logs/${sample_name}/run_simulations.%A.%a.err"
+    mkdir -p $(dirname $output_log)
+
+    # submit the job
+    sbatch --array=1 \
+        --export=$export_vars \
+        --output="${output_log}" \
+        --error="${error_log}" \
+        workflow/scripts/pipeline/simulate_mps_fast_with_blacklist/simulate_motif_pairs.qarray.qsh
+done
